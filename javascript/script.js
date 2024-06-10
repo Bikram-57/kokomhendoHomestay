96729da59fc0b24ef619bca462ec468c8bc96f53
Bikram Das <bikramdas736136@gmail.com>
2024-06-10 14:38:28 +0530

    add transition in places

--- javascript/script.js
+++ javascript/script.js
@@ -5,3 +5,31 @@ function whatsapp() {
     var apiLink = "https://api.whatsapp.com/send?phone=" + phoneNumber + "&text=" + encodeURIComponent(message);
     window.open(apiLink, "_blank");
 };
+
+  window.addEventListener('load', () => {
+    const elements = document.querySelectorAll('.right-to-left');
+    elements.forEach(element => {
+      element.classList.add('slide-in2');
+    });
+  });
+
+  window.addEventListener('load', () => {
+    const elements = document.querySelectorAll('.left-to-right');
+    elements.forEach(element => {
+      element.classList.add('slide-in');
+    });
+  });
+
+  window.addEventListener('load', () => {
+    const elements = document.querySelectorAll('.top-to-bottom');
+    elements.forEach(element => {
+      element.classList.add('slide-in-from-top');
+    });
+  });
+
+  window.addEventListener('load', () => {
+    const elements = document.querySelectorAll('.bottom-to-top');
+    elements.forEach(element => {
+      element.classList.add('slide-in-from-bottom');
+    });
+  });
